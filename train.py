2492cffaae08937e43d211dc295c4751436d487b
ChenyuanHu <hcyjob@gmail.com>
2025-03-02 12:33:53 +0800

    支持流式和非流式

--- train.py
+++ train.py
@@ -52,8 +52,10 @@ config = ModuleConfig()
 allowed_special = {"<|endoftext|>"}  # 允许的特殊token
 disallowed_special = ()  # 禁用所有特殊token的检查
 
-tprint("使用流式加载方式处理数据集，只下载和处理4-5评分范围的高质量内容...")
-dataset = load_dataset("opencsg/Fineweb-Edu-Chinese-V2.1", data_dir = "4_5", split="train", streaming=True)
+STREAMING = False  # 流式加载可以边训练边下载，避免一次性下载70G的数据集，但流式下载之后就Shuffle不了了
+SHUFFLE = True and not STREAMING
+tprint(f"使用{'流式' if STREAMING else '批量'}加载方式处理数据集，只下载和处理4-5评分范围的高质量内容...")
+dataset = load_dataset("opencsg/Fineweb-Edu-Chinese-V2.1", data_dir = "4_5", split="train", streaming=STREAMING, shuffle=SHUFFLE)
 dataset_batch = iter(dataset.batch(batch_size=batch_size))
 
 def next_x_y(device):
