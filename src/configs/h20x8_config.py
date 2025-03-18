fb086c63c9eef5b5ccafdc38ee350eb2adddafcd
ChenyuanHu <hcyjob@gmail.com>
2025-03-19 01:26:28 +0800

    fix case

--- src/configs/h20x8_config.py
+++ src/configs/h20x8_config.py
@@ -53,7 +53,7 @@ class SftConfig:
     # 样例Prompt
     case_prompts = [
         "<|im_start|>用户\n请根据规律填充这两个空缺的数字。 4, 3, 4, 3, 4, 3, （），（）\n<|im_end|>\n<|im_start|>助手\n",
-        "<|im_start|>用户\n中华人民共和国的现在的主席是谁？\n<|im_end|>\n<|im_start|>助手\n",
+        "<|im_start|>用户\n中华人民共和国的2020年的总书记是谁？\n<|im_end|>\n<|im_start|>助手\n",
         "<|im_start|>用户\n你是谁？\n<|im_end|>\n<|im_start|>助手\n"
     ]
 
