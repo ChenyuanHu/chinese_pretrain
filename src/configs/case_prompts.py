556ebee536d6b5580605c4c4fc02dc1fa204b018
ChenyuanHu <hcyjob@gmail.com>
2025-03-20 19:03:36 +0800

    add case

--- src/configs/case_prompts.py
+++ src/configs/case_prompts.py
@@ -8,5 +8,6 @@ pretrain_case_prompts = [
 sft_case_prompts = [
     "<|im_start|>用户\n请根据规律填充这两个空缺的数字。 4, 3, 4, 3, 4, 3, （），（）\n<|im_end|>\n<|im_start|>助手\n",
     "<|im_start|>用户\n中华人民共和国的2020年的总书记是谁？\n<|im_end|>\n<|im_start|>助手\n",
-    "<|im_start|>用户\n你是谁？\n<|im_end|>\n<|im_start|>助手\n"
+    "<|im_start|>用户\n你是谁？\n<|im_end|>\n<|im_start|>助手\n",
+    "<|im_start|>用户\ntoday这个单词是什么意思？\n<|im_end|>\n<|im_start|>助手\n"
 ]
