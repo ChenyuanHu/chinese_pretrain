80fac8130a6fdd122e8fef42bfb48e0cc921ff52
ChenyuanHu <hcyjob@gmail.com>
2025-03-21 18:06:55 +0800

    update 多机训练配置

---
+++ src/configs/h20x24_7b_config.py
@@ -0,0 +1,80 @@
+# 训练循环
+class TrainConfig:
+    batch_size = 1
+    num_epochs = 10000
+    steps_per_epoch = 2000  # 每个epoch训练多少批次
+    gradient_accumulation_steps = 4  # 梯度累积步数
+
+    # checkpoint config
+    save_interval_sec = 1800  # 每n秒保存一次模型
+    save_dcp_checkpoint = True
+    save_normal_checkpoint = False
+
+    compile = True
+
+# 模型参数
+class ModuleConfig:
+    block_size: int = 4096
+    vocab_size: int = 128512  # 基础词表128000，加上特殊标记后为128512
+    n_layer: int = 32
+    n_head: int = 32
+    n_embd: int = 4096
+    n_kv_head: int = 8
+    # flash attention. 可选值为 "FLASH_ATTENTION|EFFICIENT_ATTENTION|MATH|CUDNN_ATTENTION", 用竖线多选，为空禁用
+    flash_attn: str = "FLASH_ATTENTION"
+    # amp
+    dtype = "bfloat16"
+    # MLP
+    ffn_dim_multiplier: float = 1.3
+    multiple_of: int = 1024
+    # RoPE
+    rope_theta: float = 500000.0
+    use_scaled_rope: bool = True
+    use_block_checkpoint: bool = False
+
+from configs.data_sources import *
+
+class PretrainConfig:
+    datasets = [
+        {
+            "enabled": True,
+            "data": fineweb_edu_chinese_2p1,
+            "weight": 4
+        },
+        {
+            "enabled": True,
+            "data": codeparrot_clean,
+            "weight": 1
+        },
+        {
+            "enabled": True,
+            "data": zh_en_translation,
+            "weight": 0.1
+        },
+        {
+            "enabled": True,
+            "data": open_r1_math_220k,
+            "weight": 0.5
+        }
+    ]
+    
+
+class SftConfig:
+    datasets = [
+        {
+            "enabled": True,
+            "data": sft_r1_distill,
+            "weight": 1
+        }
+    ]
+
+from configs.case_prompts import *
+
+class TrainDataConfig:
+    data = PretrainConfig()
+    case_prompts = pretrain_case_prompts
+
+    # 生成样例配置
+    max_tokens = 1000
+    temperature = 0.8
+    top_k = 60
--- src/train_nnodes.sh
+++ src/train_nnodes.sh
@@ -5,6 +5,17 @@
 
 export OMP_NUM_THREADS=10
 
+# 调试信息，确认是否使用IB网卡
+# export NCCL_DEBUG=INFO
+#
+# 设置一些重要的环境变量。启动IB网卡
+# export NCCL_IB_HCA=mlx5_2,mlx5_3,mlx5_4,mlx5_5
+#
+# export NCCL_IB_DISABLE=0            # 启用 RDMA
+# export NCCL_IB_HCA=mlx5_2           # 指定 RDMA 设备
+# export NCCL_IB_GID_INDEX=3          # RoCE v2 需指定 GID 索引
+#
+
 torchrun \
 --nnodes=2 \
 --nproc_per_node=8 \
