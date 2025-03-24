8420927346ac9270eaa6809478cbcff2ef705275
ChenyuanHu <hcyjob@gmail.com>
2025-03-24 23:13:50 +0800

    优化代码结构

--- src/train.py
+++ src/train.py
@@ -91,6 +91,91 @@ class Trainer:
         tprint(f"模型总参数量(当前GPU): {total_params:,}")
         tprint(f"可训练参数量(当前GPU): {trainable_params:,}")
         tprint(f"模型总大小(当前GPU): {total_params * 4 / (1024**2):.2f} MB")  # 假设每个参数是4字节（float32）
+
+    def train_one_epoch(self, epoch):
+        self.model.train()
+        t0 = time.time()
+        total_train_loss = 0
+        total_train_tokens = 0
+        
+        self.optimizer.zero_grad()  # 在epoch开始时重置梯度
+        
+        last_print_time = time.time()
+        for step in range(self.train_config.steps_per_epoch):
+            try:
+                # 获取下一批数据
+                x, y = self.data_loader.next()
+                x = torch.tensor(x, dtype=torch.long, device=self.env.device)
+                y = torch.tensor(y, dtype=torch.long, device=self.env.device)
+                
+                # 前向传播
+                with self.amp:
+                    _, loss = self.model(x, y)
+                
+                    # 确保损失是标量
+                    loss = loss.mean()  # 添加这行来确保损失是标量
+                    
+                    # 缩放损失以适应梯度累积
+                    scaled_loss = self.amp_scaler.scale(loss / self.train_config.gradient_accumulation_steps)
+
+                scaled_loss.backward()
+                
+                # 累计损失和token数
+                total_train_loss += loss.item() * y.numel()
+                total_train_tokens += y.numel()
+                
+                # 梯度累积：每 gradient_accumulation_steps 步进行一次更新，或者最后一个step
+                if (step + 1) % self.train_config.gradient_accumulation_steps == 0 or (step + 1 == self.train_config.steps_per_epoch):
+                    self.amp_scaler.step(self.optimizer)
+                    self.amp_scaler.update()
+                    self.scheduler.step()  # 更新学习率
+                    self.optimizer.zero_grad()
+                
+                current_time = time.time()
+                if self.env.local_rank == 0 and current_time - last_print_time >= 30:  # 每30秒打印一次
+                    tokens_per_sec = total_train_tokens / (current_time - t0)
+                    current_lr = int(self.scheduler.get_last_lr()[0] * 1e5)
+                    tprint(f"Epoch {epoch+1}, Step {step+1}/{self.train_config.steps_per_epoch}, Loss: {loss.item():.4f}, LR: {current_lr}e-5, tokens/s/gpu: {tokens_per_sec:.2f}")
+                    last_print_time = current_time
+                    
+            except Exception as e:
+                tprint(f"进程 {self.env.rank} 在训练步骤中遇到错误: {str(e)}")
+                raise e
+        
+        # 在epoch结束时同步所有进程
+        self.env.barrier()
+        
+
+        total_train_loss_tensor = torch.tensor(total_train_loss, device=self.env.device)
+        total_train_tokens_tensor = torch.tensor(total_train_tokens, device=self.env.device)
+        if self.env.enabled:
+            dist.all_reduce(total_train_loss_tensor, op=dist.ReduceOp.SUM)
+            dist.all_reduce(total_train_tokens_tensor, op=dist.ReduceOp.SUM)
+        global_train_loss = total_train_loss_tensor.item()
+        global_train_tokens = total_train_tokens_tensor.item()
+        global_avg_train_loss = global_train_loss / global_train_tokens
+        global_train_ppl = torch.exp(torch.tensor(global_avg_train_loss)).item()
+
+        # 计算整个训练集群的tokens/s
+        global_tokens_per_sec = global_train_tokens / (time.time() - t0)
+
+        # 在验证集上评估
+        # global_eval_avg_loss, global_eval_ppl = self.evaluate_runner.evaluate(self.model, self.env.device, self.env)
+
+        t1 = time.time()
+        data_progress_percentage = self.data_loader.get_data_progress_percentage()
+        current_lr = int(self.scheduler.get_last_lr()[0] * 1e5)
+        tprint(f"Epoch [{epoch+1}/{self.train_config.num_epochs}], {(t1-t0):.2f}sec, "
+            f"world {global_tokens_per_sec:.2f} tokens/s, "
+            f"训练损失: {global_avg_train_loss:.4f}, 困惑度: {global_train_ppl:.4f}, "
+            f"LR: {current_lr}e-5")
+
+
+        #tprint(f"全局验证损失: {global_eval_avg_loss:.4f}, 困惑度: {global_eval_ppl:.4f}")
+        tprint(f"数据集使用度: {data_progress_percentage}")
+        
+        # 检查是否需要保存检查点
+        self.checkpoint_manager.check_save_checkpoint(self.model, self.optimizer, epoch, data_progress_percentage)
         
     def train(self):
         start_epoch, progress_percentage = self.checkpoint_manager.try_load_checkpoint(self.model, self.optimizer)
@@ -106,89 +191,7 @@ class Trainer:
         tprint(f"lr scheduler 初始化完成")
 
         for epoch in range(start_epoch, self.train_config.num_epochs):
-            self.model.train()
-            t0 = time.time()
-            total_train_loss = 0
-            total_train_tokens = 0
-            
-            self.optimizer.zero_grad()  # 在epoch开始时重置梯度
-            
-            last_print_time = time.time()
-            for step in range(self.train_config.steps_per_epoch):
-                try:
-                    # 获取下一批数据
-                    x, y = self.data_loader.next()
-                    x = torch.tensor(x, dtype=torch.long, device=self.env.device)
-                    y = torch.tensor(y, dtype=torch.long, device=self.env.device)
-                    
-                    # 前向传播
-                    with self.amp:
-                        _, loss = self.model(x, y)
-                    
-                        # 确保损失是标量
-                        loss = loss.mean()  # 添加这行来确保损失是标量
-                        
-                        # 缩放损失以适应梯度累积
-                        scaled_loss = self.amp_scaler.scale(loss / self.train_config.gradient_accumulation_steps)
-
-                    scaled_loss.backward()
-                    
-                    # 累计损失和token数
-                    total_train_loss += loss.item() * y.numel()
-                    total_train_tokens += y.numel()
-                    
-                    # 梯度累积：每 gradient_accumulation_steps 步进行一次更新，或者最后一个step
-                    if (step + 1) % self.train_config.gradient_accumulation_steps == 0 or (step + 1 == self.train_config.steps_per_epoch):
-                        self.amp_scaler.step(self.optimizer)
-                        self.amp_scaler.update()
-                        self.scheduler.step()  # 更新学习率
-                        self.optimizer.zero_grad()
-                    
-                    current_time = time.time()
-                    if self.env.local_rank == 0 and current_time - last_print_time >= 30:  # 每30秒打印一次
-                        tokens_per_sec = total_train_tokens / (current_time - t0)
-                        current_lr = int(self.scheduler.get_last_lr()[0] * 1e5)
-                        tprint(f"Epoch {epoch+1}, Step {step+1}/{self.train_config.steps_per_epoch}, Loss: {loss.item():.4f}, LR: {current_lr}e-5, tokens/s/gpu: {tokens_per_sec:.2f}")
-                        last_print_time = current_time
-                        
-                except Exception as e:
-                    tprint(f"进程 {self.env.rank} 在训练步骤中遇到错误: {str(e)}")
-                    raise e
-            
-            # 在epoch结束时同步所有进程
-            self.env.barrier()
-            
-
-            total_train_loss_tensor = torch.tensor(total_train_loss, device=self.env.device)
-            total_train_tokens_tensor = torch.tensor(total_train_tokens, device=self.env.device)
-            if self.env.enabled:
-                dist.all_reduce(total_train_loss_tensor, op=dist.ReduceOp.SUM)
-                dist.all_reduce(total_train_tokens_tensor, op=dist.ReduceOp.SUM)
-            global_train_loss = total_train_loss_tensor.item()
-            global_train_tokens = total_train_tokens_tensor.item()
-            global_avg_train_loss = global_train_loss / global_train_tokens
-            global_train_ppl = torch.exp(torch.tensor(global_avg_train_loss)).item()
-
-            # 计算整个训练集群的tokens/s
-            global_tokens_per_sec = global_train_tokens / (time.time() - t0)
-
-            # 在验证集上评估
-            # global_eval_avg_loss, global_eval_ppl = self.evaluate_runner.evaluate(self.model, self.env.device, self.env)
-
-            t1 = time.time()
-            data_progress_percentage = self.data_loader.get_data_progress_percentage()
-            current_lr = int(self.scheduler.get_last_lr()[0] * 1e5)
-            tprint(f"Epoch [{epoch+1}/{self.train_config.num_epochs}], {(t1-t0):.2f}sec, "
-                f"world {global_tokens_per_sec:.2f} tokens/s, "
-                f"训练损失: {global_avg_train_loss:.4f}, 困惑度: {global_train_ppl:.4f}, "
-                f"LR: {current_lr}e-5")
-
-
-            #tprint(f"全局验证损失: {global_eval_avg_loss:.4f}, 困惑度: {global_eval_ppl:.4f}")
-            tprint(f"数据集使用度: {data_progress_percentage}")
-            
-            # 检查是否需要保存检查点
-            self.checkpoint_manager.check_save_checkpoint(self.model, self.optimizer, epoch, data_progress_percentage)
+            self.train_one_epoch(epoch)
 
             self.env.barrier()
             # 每个epoch结束后生成示例文本
