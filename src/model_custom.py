0bfd1bf6af9cdfb592f04d065a9e77f35cf0c2aa
ChenyuanHu <hcyjob@gmail.com>
2025-04-11 00:58:01 +0800

    custom model的实现细节上与qwen2对齐

--- src/model_custom.py
+++ src/model_custom.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.attention import SDPBackend, sdpa_kernel
 from torch.utils.checkpoint import checkpoint
-from rope import RoPEv2 as RoPE
 from transformers import PretrainedConfig, PreTrainedModel
 from transformers.modeling_outputs import CausalLMOutputWithPast
 from transformers.generation import GenerationMixin
@@ -71,9 +70,103 @@ class CustomModelConfig(PretrainedConfig):
         self.use_block_checkpoint = use_block_checkpoint
         self.flash_attn = flash_attn
 
+def rotate_half(x):
+    """Rotates half the hidden dims of the input."""
+    x1 = x[..., : x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2 :]
+    return torch.cat((-x2, x1), dim=-1)
+
+def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
+    """Applies Rotary Position Embedding to the query and key tensors.
+
+    Args:
+        q (`torch.Tensor`): The query tensor.
+        k (`torch.Tensor`): The key tensor.
+        cos (`torch.Tensor`): The cosine part of the rotary embedding.
+        sin (`torch.Tensor`): The sine part of the rotary embedding.
+        position_ids (`torch.Tensor`, *optional*):
+            Deprecated and unused.
+        unsqueeze_dim (`int`, *optional*, defaults to 1):
+            The 'unsqueeze_dim' argument specifies the dimension along which to unsqueeze cos[position_ids] and
+            sin[position_ids] so that they can be properly broadcasted to the dimensions of q and k. For example, note
+            that cos[position_ids] and sin[position_ids] have the shape [batch_size, seq_len, head_dim]. Then, if q and
+            k have the shape [batch_size, heads, seq_len, head_dim], then setting unsqueeze_dim=1 makes
+            cos[position_ids] and sin[position_ids] broadcastable to the shapes of q and k. Similarly, if q and k have
+            the shape [batch_size, seq_len, heads, head_dim], then set unsqueeze_dim=2.
+    Returns:
+        `tuple(torch.Tensor)` comprising of the query and key tensors rotated using the Rotary Position Embedding.
+    """
+    cos = cos.unsqueeze(unsqueeze_dim)
+    sin = sin.unsqueeze(unsqueeze_dim)
+    q_embed = (q * cos) + (rotate_half(q) * sin)
+    k_embed = (k * cos) + (rotate_half(k) * sin)
+    return q_embed, k_embed
+
+from transformers.modeling_rope_utils import ROPE_INIT_FUNCTIONS
+
+class Qwen2RotaryEmbedding(nn.Module):
+    def __init__(self, config, device=None):
+        super().__init__()
+        # BC: "rope_type" was originally "type"
+        if hasattr(config, "rope_scaling") and config.rope_scaling is not None:
+            self.rope_type = config.rope_scaling.get("rope_type", config.rope_scaling.get("type"))
+        else:
+            self.rope_type = "default"
+        self.max_seq_len_cached = config.max_position_embeddings
+        self.original_max_seq_len = config.max_position_embeddings
+
+        self.config = config
+        self.rope_init_fn = ROPE_INIT_FUNCTIONS[self.rope_type]
+
+        inv_freq, self.attention_scaling = self.rope_init_fn(self.config, device)
+        self.register_buffer("inv_freq", inv_freq, persistent=False)
+        self.original_inv_freq = self.inv_freq
+
+    def _dynamic_frequency_update(self, position_ids, device):
+        """
+        dynamic RoPE layers should recompute `inv_freq` in the following situations:
+        1 - growing beyond the cached sequence length (allow scaling)
+        2 - the current sequence length is in the original scale (avoid losing precision with small sequences)
+        """
+        seq_len = torch.max(position_ids) + 1
+        if seq_len > self.max_seq_len_cached:  # growth
+            inv_freq, self.attention_scaling = self.rope_init_fn(self.config, device, seq_len=seq_len)
+            self.register_buffer("inv_freq", inv_freq, persistent=False)  # TODO joao: may break with compilation
+            self.max_seq_len_cached = seq_len
+
+        if seq_len < self.original_max_seq_len and self.max_seq_len_cached > self.original_max_seq_len:  # reset
+            # This .to() is needed if the model has been moved to a device after being initialized (because
+            # the buffer is automatically moved, but not the original copy)
+            self.original_inv_freq = self.original_inv_freq.to(device)
+            self.register_buffer("inv_freq", self.original_inv_freq, persistent=False)
+            self.max_seq_len_cached = self.original_max_seq_len
+
+    @torch.no_grad()
+    def forward(self, x, position_ids):
+        if "dynamic" in self.rope_type:
+            self._dynamic_frequency_update(position_ids, device=x.device)
+
+        # Core RoPE block
+        inv_freq_expanded = self.inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, 1)
+        position_ids_expanded = position_ids[:, None, :].float()
+        # Force float32 (see https://github.com/huggingface/transformers/pull/29285)
+        device_type = x.device.type
+        device_type = device_type if isinstance(device_type, str) and device_type != "mps" else "cpu"
+        with torch.autocast(device_type=device_type, enabled=False):
+            freqs = (inv_freq_expanded.float() @ position_ids_expanded.float()).transpose(1, 2)
+            emb = torch.cat((freqs, freqs), dim=-1)
+            cos = emb.cos()
+            sin = emb.sin()
+
+        # Advanced RoPE types (e.g. yarn) apply a post-processing scaling factor, equivalent to scaling attention
+        cos = cos * self.attention_scaling
+        sin = sin * self.attention_scaling
+
+        return cos.to(dtype=x.dtype), sin.to(dtype=x.dtype)
+
 
 class CausalSelfAttention(nn.Module):
-    def __init__(self, config, rope):
+    def __init__(self, config):
         super().__init__()
         # 添加头数验证
         assert config.num_attention_heads % config.num_key_value_heads == 0
@@ -87,8 +180,6 @@ class CausalSelfAttention(nn.Module):
         self.num_key_value_heads = config.num_key_value_heads
         self.hd = self.hidden_size // self.num_attention_heads
         self.n_rep = self.num_attention_heads // self.num_key_value_heads
-
-        self.rope = rope
 
         self.flash_attn_backends = []
         self.flash_attn = False
@@ -117,18 +208,18 @@ class CausalSelfAttention(nn.Module):
         self.o_proj.LLMC_RESIDUAL_SCALE_FLAG = 1
 
     @staticmethod
-    def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
-        """torch.repeat_interleave(x, dim=2, repeats=n_rep)"""
-        bs, slen, n_kv_heads, head_dim = x.shape
+    def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
+        """
+        This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
+        num_key_value_heads, seqlen, head_dim) to (batch, num_attention_heads, seqlen, head_dim)
+        """
+        batch, num_key_value_heads, slen, head_dim = hidden_states.shape
         if n_rep == 1:
-            return x
-        return (
-            x[:, :, :, None, :]
-            .expand(bs, slen, n_kv_heads, n_rep, head_dim)
-            .reshape(bs, slen, n_kv_heads * n_rep, head_dim)
-        )
+            return hidden_states
+        hidden_states = hidden_states[:, :, None, :, :].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
+        return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen, head_dim)
 
-    def forward(self, x):
+    def forward(self, x, position_embeddings):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (hidden_size)
 
         # 添加位置编码长度验证
@@ -142,15 +233,16 @@ class CausalSelfAttention(nn.Module):
         k = k.view(B, T, self.num_key_value_heads, self.hd)
         v = v.view(B, T, self.num_key_value_heads, self.hd)
 
-        q, k = self.rope.apply_rotary_emb_warp(q, k)
-
-        k = self.repeat_kv(k, self.n_rep)  # GQA <-- 2. difference compared to GPT-2
-        v = self.repeat_kv(v, self.n_rep)
-
         # 转置维度以进行注意力计算 [B, N, T, D]
         q = q.transpose(1, 2)
         k = k.transpose(1, 2)
         v = v.transpose(1, 2)
+
+        cos, sin = position_embeddings
+        q, k = apply_rotary_pos_emb(q, k, cos, sin)
+
+        k = self.repeat_kv(k, self.n_rep)  # GQA <-- 2. difference compared to GPT-2
+        v = self.repeat_kv(v, self.n_rep)
 
         if self.flash_attn:
             # flashattention
@@ -184,28 +276,28 @@ class MLP(nn.Module):
         return self.down_proj(F.silu(self.gate_proj(x)) * self.up_proj(x))
 
 class Block(nn.Module):
-    def __init__(self, config, rope, id):
+    def __init__(self, config, id):
         super().__init__()
         self.input_layernorm = nn.RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
-        self.self_attn = CausalSelfAttention(config, rope)
+        self.self_attn = CausalSelfAttention(config)
         self.post_attention_layernorm = nn.RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
         self.mlp = MLP(config)
         self.use_checkpoint = config.use_block_checkpoint
         self.id = id
 
     # 原始版本，没有显存优化
-    def forward_without_checkpoint(self, x):
-        attn_out = self.self_attn(self.input_layernorm(x))
+    def forward_without_checkpoint(self, x, position_embeddings):
+        attn_out = self.self_attn(self.input_layernorm(x), position_embeddings)
         x = x + attn_out
         mlp_out = self.mlp(self.post_attention_layernorm(x))
         x = x + mlp_out
         return x
 
-    def forward_with_checkpoint(self, x):
+    def forward_with_checkpoint(self, x, position_embeddings):
         # 重构检查点函数
         def attn_block(x):
             _x = self.input_layernorm(x)
-            return self.self_attn(_x)
+            return self.self_attn(_x, position_embeddings)
         
         def mlp_block(x):
             _x = self.post_attention_layernorm(x)
@@ -216,11 +308,11 @@ class Block(nn.Module):
         x = x + checkpoint(mlp_block, x, use_reentrant=False, preserve_rng_state=False)
         return x
 
-    def forward(self, x):
+    def forward(self, x, position_embeddings):
         if self.use_checkpoint > self.id:
-            return self.forward_with_checkpoint(x)
+            return self.forward_with_checkpoint(x, position_embeddings)
         else:
-            return self.forward_without_checkpoint(x)
+            return self.forward_without_checkpoint(x, position_embeddings)
 
 
 class CustomModelForCausalLM(PreTrainedModel, GenerationMixin):
@@ -230,15 +322,10 @@ class CustomModelForCausalLM(PreTrainedModel, GenerationMixin):
         super().__init__(config)
         self.config = config
 
-        self.rope = RoPE(
-            dim = config.hidden_size // config.num_attention_heads,
-            theta = config.rope_theta,
-            use_scaled = config.use_mrope,
-        )
-
         self.model = nn.ModuleDict(dict(
+            rotary_emb = Qwen2RotaryEmbedding(config),
             embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size),
-            layers = nn.ModuleList([Block(config, self.rope, i) for i in range(config.num_hidden_layers)]),
+            layers = nn.ModuleList([Block(config, i) for i in range(config.num_hidden_layers)]),
             norm = nn.RMSNorm(config.hidden_size, eps=config.rms_norm_eps),
         ))
         self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
@@ -284,8 +371,10 @@ class CustomModelForCausalLM(PreTrainedModel, GenerationMixin):
 
     def forward(self, input_ids, labels=None, past_key_values=None, attention_mask=None, **kwargs):
         x = self.model.embed_tokens(input_ids)
+        position_ids = torch.arange(x.shape[1], device=x.device).unsqueeze(0).expand(x.shape[0], -1)
+        position_embeddings = self.model.rotary_emb(x, position_ids)
         for block in self.model.layers:
-            x = block(x)
+            x = block(x, position_embeddings)
         x = self.model.norm(x)
 
         if labels is not None:
