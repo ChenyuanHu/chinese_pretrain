e49260ecbf2242129b03d4eaa67f750bc97d70cd
ChenyuanHu <hcyjob@gmail.com>
2025-04-10 23:15:13 +0800

    更新训练配置，增加每个epoch的训练步数，延长模型保存间隔，调整学习率和优化器参数，同时修改模型配置以适应新的结构。

--- src/configs/rtx4080s_config.py
+++ src/configs/rtx4080s_config.py
@@ -9,10 +9,21 @@ class TrainConfig:
 
     num_epochs = 10000 # 一般不结束
     scheduler_epochs = 100 # 调度器预期训练收敛需要的epoch数
-    steps_per_epoch = 2000  # 每个epoch训练多少批次
+    steps_per_epoch = 20000  # 每个epoch训练多少批次
+
+    # adamw优化器参数
+    max_lr = 5e-5
+    betas = (0.9, 0.95)
+    weight_decay = 0.01
+    eps = 1e-8
+    # 学习率调度器参数
+    pct_start = 0.1 # 预热阶段占总步数的10%
+    div_factor = 10 # 起始学习率为max_lr/10
+    final_div_factor = 10 # 最终学习率为max_lr/10
+    anneal_strategy = "cos" # 余弦退火
 
     # checkpoint config
-    save_interval_sec = 180  # 每n秒保存一次模型
+    save_interval_sec = 600  # 每n秒保存一次模型
     save_dcp_checkpoint = False
     save_normal_checkpoint = True
 
@@ -22,17 +33,11 @@ class TrainConfig:
     compile = "PARTIAL"
     run_mode = "both"
 
-# from model_qwen import QwenModel as Model
-from model_custom import CustomModelForCausalLM as Model
+from model_qwen import QwenModel as Model
+# from model_custom import CustomModelForCausalLM as Model
 from model_custom import CustomModelConfig as ModelConfig
 
-model_config = ModelConfig(
-    hidden_size=768,
-    num_hidden_layers=16,
-    num_attention_heads=16,
-    num_key_value_heads=8,
-    intermediate_size=2048,
-)
+model_config = ModelConfig()
 
 from configs.data_sources import *
 
@@ -49,12 +54,12 @@ class PretrainConfig:
             "weight": 1
         },
         {
-            "enabled": True,
+            "enabled": False,
             "data": zh_en_translation,
             "weight": 1
         },
         {
-            "enabled": True,
+            "enabled": False,
             "data": open_r1_math_220k,
             "weight": 1
         }
@@ -78,6 +83,6 @@ class TrainDataConfig:
     dataloader_mode = "packing"   # 可选值为 "padding" 或 "packing"
 
     # 生成样例配置
-    max_tokens = 100
+    max_tokens = 1000
     temperature = 0.8
     top_k = 60
